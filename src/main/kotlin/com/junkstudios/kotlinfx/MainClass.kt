c041521337dd60373ed835f797c9b604dcfdfd86
Constantine <kosta.tovstik@gmail.com>
2022-05-01 18:23:14 +0300

    optimization of imports

--- src/main/kotlin/com/junkstudios/kotlinfx/MainClass.kt
+++ src/main/kotlin/com/junkstudios/kotlinfx/MainClass.kt
@@ -2,13 +2,7 @@ package com.junkstudios.kotlinfx
 
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
-import javafx.scene.Group
-import javafx.scene.Node
-import javafx.scene.Parent
 import javafx.scene.Scene
-import javafx.scene.layout.AnchorPane
-import javafx.scene.layout.Region
-import javafx.scene.layout.VBox
 import javafx.stage.Stage
 
 class MainClass : Application() {
